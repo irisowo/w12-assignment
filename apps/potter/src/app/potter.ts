af2aed3531a4bb65892e9083e3e7ff4d23c8e538
IrisChiu <iris0106.cs06@nctu.edu.tw>
2022-05-06 01:01:49 +0800

    [Green] 2nd test case

--- apps/potter/src/app/potter.ts
+++ apps/potter/src/app/potter.ts
@@ -16,11 +16,42 @@ export class Potter {
         }
     }
 
-    get total_cost() { 
-        let cost = 0;
-        for (let id = 1; id < (this.cnt_id + 1); id++){
-            cost += this.cart[id] * 8;
-        }
+    get total_cost() { ;
+        let cost = (this.cart).reduce((accumulator, current) => {
+            return accumulator + current
+        });
+        cost *= 8;
+
+        let clonecart: number[] = new Array(6).fill(0);
+        let last_sum_cart = (this.cart).reduce((accumulator, current) => {
+            return accumulator + current
+        });
+        let iter = 5;
+        while(iter--){
+            clonecart = (this.cart).map((cnt) => {
+                return cnt > 0 ? cnt - 1: cnt;
+            });
+            const sum_clonecart = clonecart.reduce((accumulator, current) => {
+                return accumulator + current
+            });
+            switch (last_sum_cart - sum_clonecart){
+                case 5:
+                    cost -= 8 * 0.25 * 5;
+                    break;
+                case 4:
+                    cost -= 8 * 0.2 * 4;
+                    break;
+                case 3:
+                    cost -= 8 * 0.1 * 3;
+                    break;
+                case 2:
+                    cost -= 8 * 0.05 * 2;
+                    break;
+                default:
+                    break;
+            }
+            last_sum_cart = sum_clonecart
+        }       
         return cost;
     }
 
