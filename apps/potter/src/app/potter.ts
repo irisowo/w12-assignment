0557345d03530fd259a8a303daa7a62b3d97a832
IrisChiu <iris0106.cs06@nctu.edu.tw>
2022-05-06 02:04:23 +0800

    [Blue] 2nd refactor

--- apps/potter/src/app/potter.ts
+++ apps/potter/src/app/potter.ts
@@ -1,10 +1,9 @@
 export class Potter {
     cart: number[] = [];
-    cnt_id = 5; // 5 different versions of book 
     
     constructor() {
         // id 0 is useless
-        for (let id = 0; id < (this.cnt_id + 1); id++){
+        for (let id = 0; id < 6; id++){
             this.cart[id] = 0;
         }
     }
@@ -17,42 +16,52 @@ export class Potter {
     }
 
     get total_cost() { ;
-        let cost = (this.cart).reduce((accumulator, current) => {
-            return accumulator + current
-        });
-        cost *= 8;
+        let last_sum_cart = this.sum_array(this.cart);
+        let cost = last_sum_cart * 8;
 
-        let clonecart: number[] = new Array(6).fill(0);
-        let last_sum_cart = (this.cart).reduce((accumulator, current) => {
-            return accumulator + current
-        });
-        let iter = 5;
-        while(iter--){
-            clonecart = (this.cart).map((cnt) => {
+        // cloned a cart to avoid modifying this.cart
+        let clonecart = Array.from(this.cart);
+
+        for(let iter = 0; iter < 4; iter++){
+            // minus each cnt of books by one
+            clonecart = (clonecart).map((cnt) => {
                 return cnt > 0 ? cnt - 1: cnt;
             });
-            const sum_clonecart = clonecart.reduce((accumulator, current) => {
-                return accumulator + current
-            });
-            switch (last_sum_cart - sum_clonecart){
-                case 5:
-                    cost -= 8 * 0.25 * 5;
-                    break;
-                case 4:
-                    cost -= 8 * 0.2 * 4;
-                    break;
-                case 3:
-                    cost -= 8 * 0.1 * 3;
-                    break;
-                case 2:
-                    cost -= 8 * 0.05 * 2;
-                    break;
-                default:
-                    break;
-            }
+            let sum_clonecart = this.sum_array(clonecart);
+            let cnt_unique = (last_sum_cart - sum_clonecart);
+            
+            cost -= this.get_discount(cnt_unique);
             last_sum_cart = sum_clonecart
         }       
         return cost;
+    }
+
+    private sum_array(my_array: number[]){
+        let sum = my_array.reduce((accumulator, current) => {
+            return accumulator + current
+        });
+        return sum;
+    }
+
+    private get_discount(unique_cnt_books: number) {
+        let discount = 0
+        switch (unique_cnt_books){
+            case 5:
+                discount = 8 * 0.25 * 5;
+                break;
+            case 4:
+                discount = 8 * 0.2 * 4;
+                break;
+            case 3:
+                discount = 8 * 0.1 * 3;
+                break;
+            case 2:
+                discount = 8 * 0.05 * 2;
+                break;
+            default:
+                break;
+        }
+        return discount;
     }
 
 }
